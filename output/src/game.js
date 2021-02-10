3abaa2e4cd5bf45c6575df5816a45684902a8e19
jani-nykanen <jnyknn@gmail.com>
2021-02-10 11:30:13 +0200

    Background art

--- output/src/game.js
+++ output/src/game.js
@@ -6,13 +6,34 @@ var GameScene = /** @class */ (function () {
         this.cam = new Camera(128, 96);
         this.stage = new Stage(ev);
         this.objects = new ObjectManager();
+        this.cloudPos = (new Array(2)).fill(0);
     }
     GameScene.prototype.refresh = function (ev) {
+        var CLOUD_SPEED = [0.25, 0.5];
         this.objects.update(this.cam, this.stage, ev);
         this.stage.update(ev);
+        for (var i = 0; i < this.cloudPos.length; ++i) {
+            this.cloudPos[i] = (this.cloudPos[i] + CLOUD_SPEED[i] * ev.step) % 256;
+        }
+    };
+    GameScene.prototype.drawBackground = function (c) {
+        c.drawBitmap(c.getBitmap("sky"), 0, 0);
+        var bmpTrees = c.getBitmap("trees");
+        var bmpClouds = c.getBitmap("clouds");
+        var dx = Math.round(this.cam.getViewport().x / 4) % bmpTrees.width;
+        var dy = Math.round(this.cam.getViewport().y / 4) % bmpTrees.height;
+        for (var j = 0; j < this.cloudPos.length; ++j) {
+            for (var i = 0; i < 2; ++i) {
+                c.drawBitmapRegion(bmpClouds, 0, bmpClouds.height / 2 * j, bmpClouds.width, bmpClouds.height / 2, bmpClouds.width * i - this.cloudPos[j], 16);
+            }
+        }
+        for (var i = 0; i < 3; ++i) {
+            c.drawBitmap(bmpTrees, i * bmpTrees.width - dx, 16 + 192 - bmpTrees.height - dy);
+        }
     };
     GameScene.prototype.redraw = function (c) {
-        c.clear(85, 85, 85);
+        c.moveTo();
+        this.drawBackground(c);
         this.cam.computeViewport(c);
         this.stage.restrictCamera(this.cam);
         this.cam.use(c);
--- src/game.ts
+++ src/game.ts
@@ -13,26 +13,66 @@ export class GameScene implements Scene {
     private stage : Stage;
     private objects : ObjectManager;
 
+    private cloudPos : Array<number>;
+
 
     constructor(param : any, ev : GameEvent) {
 
         this.cam = new Camera(128, 96);
         this.stage = new Stage(ev);
         this.objects = new ObjectManager();
+
+        this.cloudPos = (new Array<number>(2)).fill(0);
     }
 
 
     public refresh(ev : GameEvent) {
         
+        const CLOUD_SPEED = [0.25, 0.5];
+
         this.objects.update(this.cam, this.stage, ev);
 
         this.stage.update(ev);
+
+        for (let i = 0; i < this.cloudPos.length; ++ i) {
+
+            this.cloudPos[i] = (this.cloudPos[i] + CLOUD_SPEED[i]*ev.step) % 256;
+        }
+    }
+
+
+    private drawBackground(c : Canvas) {
+
+        c.drawBitmap(c.getBitmap("sky"), 0, 0);
+
+        let bmpTrees = c.getBitmap("trees");
+        let bmpClouds = c.getBitmap("clouds");
+
+        let dx = Math.round(this.cam.getViewport().x / 4) % bmpTrees.width;
+        let dy = Math.round(this.cam.getViewport().y / 4) % bmpTrees.height;
+
+        for (let j = 0; j < this.cloudPos.length; ++ j) {
+
+            for (let i = 0; i < 2; ++ i) {
+
+                c.drawBitmapRegion(bmpClouds, 0, bmpClouds.height/2 * j,
+                    bmpClouds.width, bmpClouds.height/2, 
+                    bmpClouds.width*i - this.cloudPos[j], 16);
+            }
+        }
+
+        for (let i = 0; i < 3; ++ i) {
+
+            c.drawBitmap(bmpTrees, i*bmpTrees.width - dx, 
+                16 + 192 - bmpTrees.height - dy);
+        }
     }
 
 
     public redraw(c : Canvas) {
 
-        c.clear(85, 85, 85);
+        c.moveTo();
+        this.drawBackground(c);
 
         this.cam.computeViewport(c);
         this.stage.restrictCamera(this.cam);
